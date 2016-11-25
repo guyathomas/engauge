4f8b3c5be483522fc0e3fbedbaa8f68444a99522
Guy <guythomas721@gmail.com>
2016-11-24 17:55:37 -0800

    Add autoformatter

--- src/client/dev/components/App.jsx
+++ src/client/dev/components/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const App = () => (<p>App</p>);
+// const App = () => (<p>App</p>);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      locations: [],
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (<div />);
+  }
+}
 
 module.exports = App;
