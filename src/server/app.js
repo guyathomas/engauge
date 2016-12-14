695b1b985325ac1024367257a3d36c07ea7738aa
Guy <guythomas721@gmail.com>
2016-12-13 17:35:06 -0800

    Started with trying to find URL and taking action if not found

--- src/server/app.js
+++ src/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+// require('../db/models/index');
 const db = require('../db/models');
 
 const app = express();
@@ -19,9 +20,30 @@ db.sequelize.sync()
       throw new Error(e);
     });
 
+    // User
+    //   .findOrCreate({where: {username: 'sdepold'}, defaults: {job: 'Technical Lead JavaScript'}})
+    //   .spread(function(user, created) {
+    //     console.log(user.get({
+    //       plain: true
+    //     }))
+    //     console.log(created)
+    //       {
+    //         username: 'sdepold',
+    //         job: 'Technical Lead JavaScript',
+    //         id: 1,
+    //         createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
+    //         updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
+    //       }
+    //       created: true
+
+    //   })
+
 app.post('/api/link', (req, res) => {
-  console.log('The post req was recieved - req.data', req);
-  db.casestudy.find().then(results => console.log(results));
+  console.log('The post req was recieved - req.data', req.body);
+  console.log('The models are', Object.keys(db));
+  db.casestudy.findOrCreate({ where: { url: req.body.url } }).then((first, second) => {
+    console.log(first, second);
+  });
   res.status(200).send('Hoory, post recieved');
 });
 
