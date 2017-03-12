2e70f38e692b4d4c1883d2fa7d3db7395de15d4d
Guy <guythomas721@gmail.com>
2017-03-12 16:45:00 +1100

    Created database environment variables for testing

--- src/db/config.js
+++ src/db/config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 const rootPath = path.normalize(`${__dirname}/..`);
-console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 const env = process.env.NODE_ENV || 'development';
+
 const config = {
   development: {
     root: rootPath,
@@ -12,6 +12,27 @@ const config = {
     port: 5432,
     db: {
       database: 'engauge',
+      user: 'guy',
+      password: 'password',
+      options: {
+        host: 'database',
+        dialect: 'postgres',
+        pool: {
+          max: 100,
+          min: 0,
+          idle: 10000,
+        },
+      },
+    },
+  },
+  test: {
+    root: rootPath,
+    app: {
+      name: 'engauge',
+    },
+    port: 5432,
+    db: {
+      database: 'engauge-test',
       user: 'guy',
       password: 'password',
       options: {
