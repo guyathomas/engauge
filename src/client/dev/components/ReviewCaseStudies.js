178b2008a80809fa6223909817a287e0cab41e88
Guy <guythomas721@gmail.com>
2017-03-14 08:33:02 +1100

    Remove placeholder casestudies

--- src/client/dev/components/ReviewCaseStudies.js
+++ src/client/dev/components/ReviewCaseStudies.js
@@ -6,34 +6,7 @@ class ReviewCaseStudies extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      caseStudies: [{
-        createdAt:"2017-03-12T09:15:03.396Z",
-        id:1,
-        shortCode:"b6cba",
-        updatedAt:"2017-03-12T09:15:03.396Z",
-        url:"http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg",
-        userId:"a5cbf201-a975-4a96-ac41-79c671fd1910"},
-        {
-                createdAt:"2017-03-12T09:15:03.396Z",
-                id:2,
-                shortCode:"b6cba",
-                updatedAt:"2017-03-12T09:15:03.396Z",
-                url:"http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg",
-                userId:"a5cbf201-a975-4a96-ac41-79c671fd1910"},
-                {
-                        createdAt:"2017-03-12T09:15:03.396Z",
-                        id:3,
-                        shortCode:"b6cba",
-                        updatedAt:"2017-03-12T09:15:03.396Z",
-                        url:"http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg",
-                        userId:"a5cbf201-a975-4a96-ac41-79c671fd1910"},
-                        {
-                                createdAt:"2017-03-12T09:15:03.396Z",
-                                id:4,
-                                shortCode:"b6cba",
-                                updatedAt:"2017-03-12T09:15:03.396Z",
-                                url:"http://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg",
-                                userId:"a5cbf201-a975-4a96-ac41-79c671fd1910"}],
+      caseStudies: [],
     };
   }
 
