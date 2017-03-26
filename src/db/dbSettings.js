71644d6d72d7f1a855c1ab55a003e64a3cd288df
Guy <guythomas721@gmail.com>
2017-03-26 12:34:30 +1100

    Modified db settings for testing

--- src/db/dbSettings.js
+++ src/db/dbSettings.js
@@ -5,27 +5,28 @@ const env = process.env.NODE_ENV || 'development';
 const rootPath = path.normalize(`${__dirname}/..`);
 
 const config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'engauge',
-    },
-    port: 5432,
-    db: {
-      database: 'engauge',
-      user: 'guy',
-      password: 'tangl3w00d',
-      options: {
-        host: 'database',
-        dialect: 'postgres',
-        pool: {
-          max: 100,
-          min: 0,
-          idle: 10000,
-        },
-      },
-    },
-  },
+  //Disabled for testing
+  // development: {
+  //   root: rootPath,
+  //   app: {
+  //     name: 'engauge',
+  //   },
+  //   port: 5432,
+  //   db: {
+  //     database: 'engauge',
+  //     user: 'guy',
+  //     password: 'tangl3w00d',
+  //     options: {
+  //       host: 'database',
+  //       dialect: 'postgres',
+  //       pool: {
+  //         max: 100,
+  //         min: 0,
+  //         idle: 10000,
+  //       },
+  //     },
+  //   },
+  // },
   test: {
     root: rootPath,
     app: {
@@ -47,5 +48,27 @@ const config = {
       },
     },
   },
+  development: {
+    root: rootPath,
+    app: {
+      name: 'engauge',
+    },
+    port: 5432,
+    db: {
+      database: 'engauge',
+      user: 'guy',
+      password: 'password',
+      options: {
+        host: 'postgres-db.engaugeapp.us-west-2.elasticbeanstalk.com',
+        dialect: 'postgres',
+        pool: {
+          max: 100,
+          min: 0,
+          idle: 10000,
+        },
+      },
+    },
+  },
 };
+console.log('The DB settings in the export are', config[env]);
 module.exports = config[env];
