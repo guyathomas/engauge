5855856f56152ae48d18c5ba7cd11ac66f39f278
Guy <guythomas721@gmail.com>
2017-03-27 10:10:11 +1100

    Make environment variables public since they are only for dev anyway

--- src/db/dbSettings.js
+++ src/db/dbSettings.js
@@ -2,32 +2,41 @@ const path = require('path');
 
 console.log(`The environment is ${process.env.NODE_ENV}`);
 console.log(`The environment variables are ${JSON.stringify(process.env)}`);
-const env = process.env.NODE_ENV || 'development';
+let env;
+if (process.env.NODE_ENV) {
+  env = process.env.NODE_ENV;
+} else if (process.env.RDS_USERNAME) {
+  // TODO: This is kind of hacky. Instead just set the prod environment variable in AWS
+  env = 'production';
+} else {
+  env = 'development';
+}
+
 const rootPath = path.normalize(`${__dirname}/..`);
 
 const config = {
   //Disabled for testing
-  // development: {
-  //   root: rootPath,
-  //   app: {
-  //     name: 'engauge',
-  //   },
-  //   port: 5432,
-  //   db: {
-  //     database: 'engauge',
-  //     user: 'guy',
-  //     password: 'tangl3w00d',
-  //     options: {
-  //       host: 'database',
-  //       dialect: 'postgres',
-  //       pool: {
-  //         max: 100,
-  //         min: 0,
-  //         idle: 10000,
-  //       },
-  //     },
-  //   },
-  // },
+  development: {
+    root: rootPath,
+    app: {
+      name: 'engauge',
+    },
+    port: 5432,
+    db: {
+      database: 'engauge',
+      user: 'user',
+      password: 'password',
+      options: {
+        host: 'database',
+        dialect: 'postgres',
+        pool: {
+          max: 100,
+          min: 0,
+          idle: 10000,
+        },
+      },
+    },
+  },
   test: {
     root: rootPath,
     app: {
@@ -49,7 +58,7 @@ const config = {
       },
     },
   },
-  development: {
+  production: {
     root: rootPath,
     app: {
       name: 'engauge',
