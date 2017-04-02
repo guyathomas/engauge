43190f7c1a50f4c43fba2906e7bbd021d6e45a9c
Guy <guythomas721@gmail.com>
2017-04-02 18:12:25 +1000

    Implement header logo

--- src/client/dev/components/HeaderLinks.js
+++ src/client/dev/components/HeaderLinks.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+const Logo = (location) => {
+  if (location === 'landing-nav') {
+    return (<div className="hidden"/>);
+  } else {
+    return (
+    <div className="logo">
+      <Link to="/">
+        <img src={'../../assets/images/brand.png'} />
+      </Link>
+    </div>);
+  }
+};
+
 const HeaderLinks = props => (
   <nav className={props.location}>
-    <div><Link to="/">Home</Link></div>
-    <div><Link to="/watch">Watch</Link></div>
+    {Logo(props.location)}
+    <div className="first-item"><Link to="/watch">Watch</Link></div>
     <div><Link to="/review">Review</Link></div>
   </nav>
 );
