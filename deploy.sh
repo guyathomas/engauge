acd9e04b8a07598a54aecbc11ab2ddda06099e27
Guy <guythomas721@gmail.com>
2017-04-25 18:05:23 +1000

    Fix application name

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ aws configure set aws_secret_access_key $SECRET
 # aws configure set aws_secret_access_key default_secret_key
 
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name engauge \
+aws elasticbeanstalk create-application-version --application-name engauge-app \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
