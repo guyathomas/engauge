a4c7872f295abdad6eacc98ad8145d715c0d062e
Guy <guythomas721@gmail.com>
2017-04-27 18:07:23 +1000

    Revert to last working state

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,13 @@
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+process.env.HOST = process.env.HOST || '0.0.0.0';
+process.env.PORT = process.env.PORT || 3000;
+
+const server = app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`App listening on port ${process.env.PORT}!`);
+  console.log(`The full server details ${JSON.stringify(server.address())}`);
+  console.log(`Launched at: ${new Date()}!`);
 });
 
 const socketHandler = require('./socketHandler');
