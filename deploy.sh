4fe15f8d839949e3b51e5777b3e894e2500b3115
Guy <guythomas721@gmail.com>
2017-04-30 18:25:36 +1000

    Add ebextensions to deploy script and also create EB extensions file

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ SECRET=$3
 # Create new Elastic Beanstalk version
 EB_BUCKET=engauge-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+EBEXTENSIONS=.ebextensions
 
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws configure set default.region us-west-1
@@ -16,9 +17,11 @@ aws configure set aws_access_key_id $ACCESS_ID
 aws configure set aws_secret_access_key $SECRET
 # aws configure set aws_secret_access_key default_secret_key
 
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+zip -r package.zip $DOCKERRUN_FILE $EBEXTENSIONS
+aws s3 cp package.zip s3://$EB_BUCKET/$DOCKERRUN_FILE
+
 aws elasticbeanstalk create-application-version --application-name engauge-app \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=package.zip
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name engauge-prod \
