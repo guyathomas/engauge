235c87e4c726c46fa91004cb99d817ac5fa0249e
Guy <guythomas721@gmail.com>
2017-04-30 18:36:12 +1000

    Fix incorrect naming of copied file on S3

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ aws configure set aws_secret_access_key $SECRET
 # aws configure set aws_secret_access_key default_secret_key
 
 zip -r package.zip $DOCKERRUN_FILE $EBEXTENSIONS
-aws s3 cp package.zip s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws s3 cp package.zip s3://$EB_BUCKET/package.zip
 
 aws elasticbeanstalk create-application-version --application-name engauge-app \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=package.zip
