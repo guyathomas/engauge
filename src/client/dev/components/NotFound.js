2dd873d44a45c264b71be430b2bcee4883dbf3f2
Guy <guythomas721@gmail.com>
2017-05-30 15:57:03 +1000

    Change component to not be stateful

--- src/client/dev/components/NotFound.js
+++ src/client/dev/components/NotFound.js
@@ -1,23 +1,9 @@
 import React from 'react';
 
-class NotFound extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dummy: '',
-    };
-  }
-
-
-  render() {
-  	console.log(this.props)
-    return (
-      <div>
-    		<div>Not Found</div>
-        <div>{JSON.stringify(this.props)}</div>
-      </div>
-    );
-  }
-}
+const NotFound = () => (
+  <div>
+    <div>Not Found</div>
+  </div>
+);
 
 module.exports = NotFound;
