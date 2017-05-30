8174f66284bd551a433a39bcaf588d09d66e8897
Guy <guythomas721@gmail.com>
2017-05-30 16:44:59 +1000

    Hook the store up to react router

--- src/client/dev/components/App.js
+++ src/client/dev/components/App.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
-import Landing from './Landing';
-import Watch from './Watch';
-import Review from './Review';
-import ReviewSidebar from './ReviewSidebar';
-import NotFound from './NotFound';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
 import PrimaryHeader from './PrimaryHeader';
-import CSList from './CSList';
 
-const WatchList = () => (<CSList action="watch" />);
-const ReviewList = () => (<CSList action="review" />);
+function mapStateToProps(state) {
+  return {
+    // posts: state.posts,
+    // comments: state.comments
+  }
+}
 
-const App = () => (
-  <Router history={browserHistory}>
-    <Route path="/" component={Landing} />
-    <Route component={PrimaryHeader}>
-      <Route path="/watch/:shortCode" component={Watch} />
-      <Route path="/watch" component={WatchList} />
-      <Route path="/review" component={ReviewList} />
-      <Route path="/review/:shortCode" component={ReviewSidebar} >
-        <Route path="/review/:shortCode/:sessionID" component={Review} />
-      </Route>
-    </Route>
-    <Route path="*" component={NotFound} />
-  </Router>
-);
+function mapDispachToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
 
-module.exports = App;
+const App = connect(mapStateToProps, mapDispachToProps)(PrimaryHeader);
+
+export default App;
--- src/client/dev/index.jsx
+++ src/client/dev/index.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
 
-render(<App />, document.getElementById('app'));
+// Import css
+// import css from './styles/style.styl';
+
+// Import Components
+import App from './components/App';
+import Landing from './components/Landing';
+import Watch from './components/Watch';
+import Review from './components/Review';
+import ReviewSidebar from './components/ReviewSidebar';
+import NotFound from './components/NotFound';
+// import PrimaryHeader from './components/PrimaryHeader';
+import CSList from './components/CSList';
+
+import store, { history } from './store';
+
+const WatchList = () => (<CSList action="watch" />);
+const ReviewList = () => (<CSList action="review" />);
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Landing} />
+      <Route component={App}>
+        <Route path="/watch/:shortCode" component={Watch} />
+        <Route path="/watch" component={WatchList} />
+        <Route path="/review" component={ReviewList} />
+        <Route path="/review/:shortCode" component={ReviewSidebar} >
+          <Route path="/review/:shortCode/:sessionID" component={Review} />
+        </Route>
+      </Route>
+      <Route path="*" component={NotFound} />
+    </Router>
+  </Provider>
+);
+
+render(router, document.getElementById('app'));
