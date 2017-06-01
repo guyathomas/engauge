343db8a2293ac1717fa7caddf21c401633cd1a95
Guy <guythomas721@gmail.com>
2017-06-01 20:10:54 +1000

    Session count now comes through correctly

--- src/client/dev/components/StudyCard.js
+++ src/client/dev/components/StudyCard.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const StudyCard = props => (
-  <div className="study" onClick={props.selectStudy}>
-  	<div className="details">
-		<div className="title">{props.title || 'Cute Puppy'}</div>
-	    <div className="session-count">{`${props.sessionCount || 0} sessions`}</div>
-  	</div>
-    <div className="imgwrapper">
-      <img src={props.study.url} />
+const StudyCard = props => {
+  const sessionCount = props.study.sessions.length;
+  const sessionString = `${sessionCount} ${sessionCount === 1 ? 'session' : 'sessions'}`;
+  return (
+    <div className="study" onClick={props.selectStudy}>
+    	<div className="details">
+  		<div className="title">{props.title || 'Cute Puppy'}</div>
+  	    <div className="session-count">{sessionString}</div>
+    	</div>
+      <div className="imgwrapper">
+        <img src={props.study.url} />
+      </div>
     </div>
-  </div>
-);
+  )
+}
 
 module.exports = StudyCard;
