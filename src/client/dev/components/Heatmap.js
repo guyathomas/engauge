21471f4c2c620b55083d61fd076b6b3a86869e77
Guy <guythomas721@gmail.com>
2017-06-08 17:42:00 +1000

    Clean up logging

--- src/client/dev/components/Heatmap.js
+++ src/client/dev/components/Heatmap.js
@@ -16,8 +16,6 @@ class Heatmap extends React.Component {
     
     const unsortedSessions = pullKeyFromObjArr(selectedSessions, sessions, 'recording');
     const aggregateData = mergeNArrays(unsortedSessions, (a, b) => (a && b) && (a.time < b.time));
-    console.log(JSON.stringify(aggregateData))
-      console.log('About to pass this data to render on heatmap',aggregateData )
         if (aggregateData.length > 0) {
           const heatMapData = {
             max: 2,
