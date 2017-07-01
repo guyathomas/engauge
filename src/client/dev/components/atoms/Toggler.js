2ed2ae9a266decb18e95be0c52dd4a8d242e6a21
Guy <guythomas721@gmail.com>
2017-07-01 14:53:38 -0700

    Make slightly more responsive

--- src/client/dev/components/atoms/Toggler.js
+++ src/client/dev/components/atoms/Toggler.js
@@ -15,8 +15,8 @@ class Toggler extends React.Component {
     return (
       <div className="toggler" onClick={this.toggleActive.bind(this)}>
         <div style={{ [this.state.side]: 0 }} className="highlighted" />
-        <div className={`left${this.state.side === 'left' ? ' active' : ''}`}>Show All Studies</div>
-        <div className={`right${this.state.side === 'right' ? ' active' : ''}`}>Show Only Mine</div>
+        <div className={`left${this.state.side === 'left' ? ' active' : ''}`}>All Studies</div>
+        <div className={`right${this.state.side === 'right' ? ' active' : ''}`}>Only Mine</div>
       </div>
     );
   }
