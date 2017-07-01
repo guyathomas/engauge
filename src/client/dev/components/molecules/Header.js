08600c5e91d4d96179014fbdee2c416c8a6982a0
Guy <guythomas721@gmail.com>
2017-07-01 12:36:31 -0700

    Change header to use id's

--- src/client/dev/components/molecules/Header.js
+++ src/client/dev/components/molecules/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const Header = (props) => (
-  <nav className={"primary-nav"}>
+  <nav id="primary-nav">
     <div className="logo">
       <Link to="/">
         <img src={'../../assets/images/brand.png'} />
